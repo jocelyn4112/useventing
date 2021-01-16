2d48b706bbe9c7656bf38ceb3a7098ee71fa1b34
jocelyn4112 <68340393+jocelyn4112@users.noreply.github.com>
2021-01-16 16:51:33 -0500

    JS to PDF

--- example.js
+++ example.js
@@ -5,8 +5,8 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://evententries.com/#LiveScores$5stWag00r1gyTkXDbonSw==');
-  await page.screenshot({path: 'example.png'});
+  await page.goto('https://evententries.com/#LiveScores$5stWag00r1gyTkXDbonSw==', {waitUntil: 'networkidle2'}););
+  await page.screenshot({path: 'Event1.png'});
 
   await browser.close();
 })();
--- hn.js
+++ hn.js
@@ -1,10 +1,12 @@
+
+// To scrap JS pages using puppeter - turns them into PDF
 const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://evententries.com/#LiveScores$5stWag00r1gyTkXDbonSw==', {waitUntil: 'networkidle2'});
-  await page.pdf({path: 'hn.pdf', format: 'A4'});
+  await page.content({path: 'Event1.html', format: 'A4'});
 
   await browser.close();
 })();
