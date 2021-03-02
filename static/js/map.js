0822a73431dd9c85bcc457693e8818b557dc481e
mmegancross <66575425+mmegancross@users.noreply.github.com>
2021-03-02 09:26:33 -0500

    map and vis3 updates

--- static/js/map.js
+++ static/js/map.js
@@ -1,34 +1,91 @@
 // Creating map object
-var myMap = L.map("map", {
-      center: [40.7128, -74.0059],
-      zoom: 11
-    });
+    var myMap = L.map("map").setView([37.0902, -95.7129], 4);
     
     // Adding tile layer
-    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      tileSize: 512,
-      maxZoom: 18,
-      zoomOffset: -1,
-      id: "mapbox/light-v10",
-      accessToken: API_KEY
+    // L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    //   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    //   tileSize: 512,
+    //   maxZoom: 18,
+    //   zoomOffset: -1,
+    //   id: "mapbox/light-v10",
+    //   accessToken: API_KEY
+    // }).addTo(myMap);
+    
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/light-v10',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: API_KEY
     }).addTo(myMap);
-    
-    // //Use this link to get the csv data.
-    // var link = "static/data/event_map_dataset.csv";
-    
-    // // Grabbing our CSV data..
-    // d3.json(link, function(data) {
-    //   // Creating a layer with the retrieved data
-    //   L.circleMarker([row.Lat, row.Lon], {
-    //     opacity: 1,
-    //     color: chocolor,
-    //     //fillColor: chocolor,
-    //     //fillOpacity: 0.25,
-    //     //radius: rad
-    // }).bindPopup(row.Event);                
-    // }; 
 
+    
+    //convert Json to GeoJson 
+
+    var data = "../data/event_map_dataset.json";
+
+    var jsonFeatures = [];
+    data.forEach(function(point){
+    var lat = point.Latitude;
+    var lon = point.Longitude;
+    var feature = {type: 'Feature',
+        properties: point,
+        geometry: {
+            type: 'Point',
+            coordinates: [lon,lat]
+        },
+    };
+    jsonFeatures.push(feature);
+});
+var geoJson = { type: 'FeatureCollection', features: jsonFeatures };
+    
+L.geoJson(geoJson).addTo(myMap);   
+
+
+// //Use this link to get the json data.
+
+
+
+    
+    // // Grabbing our json data..
+    // d3.json(link, function(data) {
+    //     L.circleMarker((["Latitude"],["Longitude"]), {
+    //         opacity: 1, 
+    //         color: white,
+    //     }).bindPopup(["Title"]).addTo(myMap);
+    // }); 
+
+    // // Creating a layer with the retrieved data
+    //     L.circleMarker([row.Lat, row.Lon], {
+    //         opacity: 1,
+    // //     color: chocolor,
+    // //     //fillColor: chocolor,
+    // //     //fillOpacity: 0.25,
+    // //     //radius: rad
+    // }).bindPopup(row.Event);                
+    //  }; 
+
+
+
+
+
+//papa parse method 
+
+// Read markers data from data.csv
+//     $.get('static/data/event_map_dataset.csv', function(csvString) {
+//         // Use PapaParse to convert string to array of objects
+//         var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;
+//         // For each row in data, create a marker and add it to the map
+//         // For each row, columns `Latitude`, `Longitude`, and `Title` are required
+//         for (var i in data) {
+//             var row = data[i];
+//             var marker = L.circleMarker([row.Latitude, row.Longitude], {
+//             opacity: 1
+//             }).bindPopup(row.Title);
+//             marker.addTo(myMap);
+//         }
+//   });
     
 
 //}).bindPopup(row.Title);
